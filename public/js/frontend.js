1579a5685535916259e0414ad11d696c8534e954
Nathan Hyman <n.hyman94@gmail.com>
2015-05-16 20:51:10 +1000

    missed public folder

---
+++ public/js/frontend.js
@@ -0,0 +1,133 @@
+var socket = io();
+var username='';
+var typing = false;
+var timeout = undefined;
+
+
+function timeoutFunction() {
+    typing = false;
+    socket.emit("typing",false);
+}
+
+
+if (username =='') {
+    console.log('not set');
+    $('#chat').hide();
+}
+$('#login').submit(function() {
+    var username=santize($('#username').val());
+
+    socket.emit('login', username);
+    $('#username').val('');
+    $('#login').hide();
+    $('#chat').show();
+    return false;
+});
+$('#m').keyup(function(e){
+    if (e.which !== 13) {
+        if (typing === false && $("#m").is(":focus") || $('#m').val() != '') {
+            typing = true;
+            socket.emit("typing",true);
+            console.log("typing!");
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(timeoutFunction,5000);
+            console.log("reset timeout");
+        }
+    }
+});
+
+socket.on("isTyping",function(data){
+    console.log("typing function beg");
+    console.log(data);
+    if ($("#updates").children().length==0) {
+        $("#typing").css("display",'none');
+    } else{
+        $("#typing").css("display",'inline');
+    }
+    console.log(data.isTyping);
+    if (data.isTyping) {
+        console.log('istyping is true');
+        if ($("#"+data.person+"").length === 0){
+            console.log('data.person length is 0');
+            console.log(data.person);
+            $("#updates").append("<span id='"+ data.person +"'>" + data.person + " <span>");
+            console.log("isTyping Function");
+            timeout = setTimeout(timeoutFunction,5000);
+        }
+    } else {
+        $("#"+data.person+"").remove();
+    }
+     if ($("#updates").children().length==0) {
+        console.log('length is 0, hiding typing')
+        $("#typing").css("display",'none');
+    } else{
+        console.log('length is not 0,showing typing')
+        $("#typing").css("display",'inline');
+    }
+});
+
+// socket.on('send history',function(data){
+//     console.log("history is " + data);
+
+// });
+
+$('#chatinput').submit(function() {
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+
+
+
+socket.on('chat message', function(data){
+    console.log("message is " + data);
+    var line = data.author + ":" + data.text;
+    if (data.type == 'system') {
+        addMessage(data.author,data.text,data.colour);
+    } else {
+        addMessage(data.author,data.text,data.colour,data.time);
+    }
+    console.log('recieving msg');
+    $("#"+data.author+"").remove();
+    // clearTimeout(timeout);
+    // time = setTimeout(timeoutFunction,0);
+    console.log('finishing msg');
+});
+
+
+socket.on('login', function(msg){
+    $('#messages').append($('<li>').text(msg));
+    $('#messages').animate({
+        scrollTop: $('#messages').scrollHeight
+    },300);
+});
+socket.on('users',function(list){
+    console.log('Updating online users');
+    $('#online li').remove();
+    for (var i = list.length - 1; i >= 0; i--) {
+        var text = '<a href="user.html"> <li>' + list[i]['username'] + '</li></a>';
+            // console.log(text);
+            $('#online').append(text);
+        };
+    });
+
+function addMessage(author,text,colour,time){
+    console.log(author + text + time + colour);
+    if (typeof time === 'undefined') {
+        console.log("showing welcome msg");
+        $('#messages').append('<li> <span style="color:black">' + text + "</span> <span style='color:" + colour + "'>" + author + "</span></li>");    
+    } else {
+        console.log(time);
+        timestamp = time.split(' ');
+        timestamp = timestamp[4];
+        console.log(timestamp);
+        $('#messages').append('<li>' + timestamp +  "<span style='color:" + colour + "'> " +  author + "</span>:" + '<span style="color:black">' + text);
+    }
+}
+
+
+function santize(input) {
+    var output = input.replace( /(,|<|\.|>|\/|\?|\;|\:|\'|\"|\{|\[|\]|})/g, "\\$1" );
+    return output;
+}
